56bd1bb891e59994ff4b4b065c8c7a78bddc41dd
Nadya Marchanka <nadia.besarab@gmail.com>
2020-05-26 21:36:55 +0300

    feat: add Yandex API

--- src/js/LocationComponent.js
+++ src/js/LocationComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Image from 'react-bootstrap/Image';
+import { YMaps, Map } from 'react-yandex-maps';
 // Import images
 function importAll(r) {
   r.keys().forEach(r);
@@ -11,7 +12,10 @@ class LocationComponent extends Component {
     return (
       <>
       <div className="location-block">
-        <Image src="images/test-map.png" fluid/>
+      <YMaps>
+        <Map defaultState={{ center: [55.75, 37.57], zoom: 9 }} />
+      </YMaps>
+        {/* <Image src="images/test-map.png" fluid/> */}
         <p className="coordinates">Latitude: 53°54', Longitude: 27°34'</p>
       </div>
       </>
