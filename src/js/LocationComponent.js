920051931f8696161afbc9f446d40aeeb0e2c23c
Nadya Marchanka <nadia.besarab@gmail.com>
2020-06-01 20:04:48 +0300

    feat: add map of current location

--- src/js/LocationComponent.js
+++ src/js/LocationComponent.js
@@ -1,8 +1,5 @@
 import React, { Component } from "react";
-import Image from 'react-bootstrap/Image';
-import { YMaps, Map, Placemark, SearchControl } from 'react-yandex-maps';
-import IPinfo from 'node-ipinfo';
-import Helper from './Helper';
+import ReactMapGL, { GeolocateControl, NavigationControl} from 'react-map-gl';
 // Import images
 function importAll(r) {
   r.keys().forEach(r);
@@ -13,71 +10,59 @@ class LocationComponent extends Component {
 
   constructor() {
     super();
-    // let today = new Date(),
-    //         date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate() + '-' + today.now();
     this.state = { 
       city: 'Default',
-      time: Date.now()
+      time: Date.now(),
+      viewport: {
+        width: 290,
+        height: 420,
+        latitude: 53.9000,
+        longitude: 27.5667,
+        zoom: 8
+      }
     };
   }
-
-  // geocode(ymaps) {
-  //   ymaps.geocode('Мытищи')
-  //     .then(result => this.setState({ coordinates: result.geoObjects.get(0).geometry.getCoordinates() }))
-  // }
-
-  // async fetchLocation() {
-  //   const URL = `https://ipinfo.io/json`;
-  //   await Helper.fetchPost(URL)
-  //     .then((content) => console.log(content))
-  //     .then((content) => console.log(content))
-  //     .catch((error) => {
-  //       throw new Error(`${error}: Problems with API`);
-  //     });
-  // }
 
   componentDidMount() {
     fetch(`https://ipinfo.io/json?token=0add77f89947b1`)
       .then(res => res.json())
-      // .then(json => console.log(json.city))
-      .then(json => this.setState({ city: json.city }));
+      .then(json => this.setState({ 
+        city: json.city ,
+        latitude: json.loc.slice(0,5),
+        longitude: json.loc.slice(8,13)
+      }));
   }
 
-  // geocode(ymaps) {
-  //   ymaps.geocode('Минск', { results: 1})
-  //     .then((res) => {
-  //           var firstGeoObject = res.geoObjects.get(0),
-  //               coords = firstGeoObject.geometry.getCoordinates(),
-  //               bounds = firstGeoObject.properties.get('boundedBy');
-
-            // firstGeoObject.options.set('preset', 'islands#darkBlueDotIconWithCaption');
-            // firstGeoObject.properties.set('iconCaption', firstGeoObject.getAddressLine());
-            // myMap.geoObjects.add(firstGeoObject);
-            // myMap.setBounds(bounds, {
-            //     checkZoomRange: true
-    // })
-  //   })
-  // }
+toNormalView(num) {
+  return `${Math.trunc(num)}°${num.toString().split('.')[1].slice(0,2)}'`;
+}
 
   render() {
+    const geolocateStyle = {
+      float: "left",
+      margin: "10px"
+    };
+
     return (
       <>
       <div className="location-block">
-        {/* <YMaps query={{ lang: 'en_RU', apikey: '6505618d-7100-448c-af26-75f6ee70583a'}}>
-           <Map width={'auto'} height={800} modules={["geocode"]} onLoad={ymaps => {
-                                this.geocode(ymaps);
-                            }}> */}
-            {/* <SearchControl state={{ request: 'Минск', }} options={{ provider: 'yandex#search', float: 'left', placeholderContent: 'Аптеки' }} /> */}
-
-
-            {/* { !this.state.coordinates ? null :
-              <Placemark geometry={{ coordinates: this.state.coordinates}} />
-            } */}
-{/* 
-          </Map>
-        </YMaps> */}
-        <Image src="images/test-map.png" fluid/>
-        <p className="coordinates">Latitude: 53°54', Longitude: 27°34'</p>
+        <ReactMapGL 
+        style={{margin: '0 auto'}}
+        {...this.state.viewport}
+        onViewportChange={(viewport) => this.setState({viewport})}
+        mapboxApiAccessToken={'pk.eyJ1Ijoia3JvdG9rb24iLCJhIjoiY2thd2R0YTU1MDR0ZDJ5bXpyd2lidmVkZSJ9.gKBq_wdxx-DJ4FH6aHbj8Q'}
+        mapStyle="mapbox://styles/mapbox/streets-v11">
+           <GeolocateControl
+            style={geolocateStyle}
+            positionOptions={{ enableHighAccuracy: true }}
+            trackUserLocation={true}
+        />
+        <div style={{position: 'absolute', right: 0}}>
+          <NavigationControl />
+        </div>
+        </ReactMapGL>
+        {/* <Image src="images/test-map.png" fluid/> */}
+        <p className="coordinates">Latitude: {this.toNormalView(this.state.viewport.latitude)}, Longitude: {this.toNormalView(this.state.viewport.longitude)}</p>
       </div>
       </>
     );
